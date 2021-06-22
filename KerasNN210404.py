- made a big mistake in term of input normalization (axis=1 option for no reason)

KerasNN210404.py
@@ -1,4 +1,4 @@
-import sys, os, time
+import sys, os, time, math
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -7,14 +7,25 @@ import matplotlib.image as mpimg
 import random
 import pickle
 
+
+def stand2dArray(array):    #following tf.image.per_image_standardization
+    array = np.array(array);
+    flatArr = array.flatten();
+    mean = np.mean(flatArr);
+    std = np.std(flatArr);
+    return (array - mean)/max(std, 1/math.sqrt(flatArr.size));
+
+
 if __name__ == "__main__":
+    printFigN = 20;
+
     fashionData = tf.keras.datasets.fashion_mnist;
     [[trainX, trainY], [testX, testY]] = fashionData.load_data();
     nameY = ["T-shirt", "Trouser", "Pullover", "Dress", "Coat",\
              "Sandal", "Shirt", "Sneaker", "Bag", "Boot"];
-    trainXNorm = tf.keras.utils.normalize(trainX, axis=1);
-    testXNorm  = tf.keras.utils.normalize(testX, axis=1);  
- 
+    trainXNorm = np.array([stand2dArray(X) for X in trainX]);
+    testXNorm  = np.array([stand2dArray(X) for X in testX]);
+
     exepath = os.path.dirname(os.path.abspath(__file__));
     print("Saving the following figures:");
     ''' 
@@ -65,7 +76,7 @@ if __name__ == "__main__":
 
     predValY = model.predict(testXNorm);
     predY = np.argmax(predValY, axis=-1);
-    for i, valX in enumerate(testX):
+    for i, valX in enumerate(testX[:printFigN]):
         print(i, nameY[predY[i]], nameY[testY[i]]);
         plt.imshow(valX, cmap=plt.cm.binary);
         plt.title("Prediction: "+nameY[predY[i]], fontsize=24);

KerasNN210410.py
@@ -7,6 +7,12 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pickle
 
+def stand2dArray(array):    #following tf.image.per_image_standardization
+    array = np.array(array);
+    flatArr = array.flatten();
+    mean = np.mean(flatArr);
+    std = np.std(flatArr);
+    return (array - mean)/max(std, 1/math.sqrt(flatArr.size));
 def buildModel(targetN=10, input_shape=[28, 28], \
                hiddenN=2, neuronN=100, learningRate=0.01):
     model = tf.keras.models.Sequential();
@@ -26,8 +32,8 @@ if __name__ == "__main__":
     [[trainX, trainY], [testX, testY]] = fashionData.load_data();
     nameY = ["T-shirt", "Trouser", "Pullover", "Dress", "Coat",\
              "Sandal", "Shirt", "Sneaker", "Bag", "Boot"];
-    trainXNorm = tf.keras.utils.normalize(trainX, axis=1);
-    testXNorm  = tf.keras.utils.normalize(testX, axis=1);  
+    trainXNorm = np.array([stand2dArray(X) for X in trainX])
+    testXNorm  = np.array([stand2dArray(X) for X in testX])
     '''
     #modeling
     epochN = 30;

KerasNN210413.py
@@ -13,6 +13,12 @@ from tensorflow.keras.activations import *
 from skopt import gp_minimize
 from skopt.space import Real, Categorical, Integer
 
+def stand2dArray(array):    #following tf.image.per_image_standardization
+    array = np.array(array);
+    flatArr = array.flatten();
+    mean = np.mean(flatArr);
+    std = np.std(flatArr);
+    return (array - mean)/max(std, 1/math.sqrt(flatArr.size));
 def buildModel(denseLayerN, denseNeuronN, learningRate, activationFunc,\
                targetN, inputShape):
     model = tf.keras.models.Sequential();
@@ -70,8 +76,8 @@ if __name__ == "__main__":
     [[trainX, trainY], [testX, testY]] = fashionData.load_data();
     nameY = ["T-shirt", "Trouser", "Pullover", "Dress", "Coat",\
              "Sandal", "Shirt", "Sneaker", "Bag", "Boot"];
-    trainXNorm = tf.keras.utils.normalize(trainX, axis=1);
-    testXNorm  = tf.keras.utils.normalize(testX, axis=1);  
+    trainXNorm = np.array([stand2dArray(X) for X in trainX]);
+    testXNorm  = np.array([stand2dArray(X) for X in testX]);
     targetN = 10;
     inputShape = [28, 28];
     '''

KerasNN210416.py
@@ -15,6 +15,14 @@ import tensorflow.keras.backend as K
 from skopt import gp_minimize
 from skopt.space import Real, Categorical, Integer
 
+
+def stand2dArray(array):    #following tf.image.per_image_standardization
+    array = np.array(array);
+    flatArr = array.flatten();
+    mean = np.mean(flatArr);
+    std = np.std(flatArr);
+    return (array - mean)/max(std, 1/math.sqrt(flatArr.size));
+
 MODELNAME="mnistFashion210416.model";
 LOWLAYERTHRES=3;
 def buildModel(denseLayerN, denseNeuronN, learningRate, actFunc, initFunc,\
@@ -130,8 +138,9 @@ if __name__ == "__main__":
     [[inputX, inputY], [testX, testY]] = fashionData.load_data();
     nameY = ["T-shirt", "Trouser", "Pullover", "Dress", "Coat",\
              "Sandal", "Shirt", "Sneaker", "Bag", "Boot"];
-    inputXNorm = tf.keras.utils.normalize(inputX, axis=1);
-    testXNorm  = tf.keras.utils.normalize(testX, axis=1);  
+    inputXNorm = np.array([stand2dArray(X) for X in inputX]);
+    testXNorm  = np.array([stand2dArray(X) for X in testX]);
+
     targetN = 10;
     inputShape = [28, 28];
     '''

KerasNN210422.py
@@ -23,7 +23,12 @@ from skopt.space import Integer, Real, Categorical
 
 MODELNAME="mnistFashion210422.model";
 
-
+def stand2dArray(array):    #following tf.image.per_image_standardization
+    array = np.array(array);
+    flatArr = array.flatten();
+    mean = np.mean(flatArr);
+    std = np.std(flatArr);
+    return (array - mean)/max(std, 1/math.sqrt(flatArr.size));
 def buildModel(convLayerN, convFilterN, denseLayerN, denseNeuronN,\
                learningRate, actFunc, initFunc,\
                targetN, inputShape, dropoutMCSeed=0):
@@ -150,8 +155,8 @@ if __name__ == "__main__":
     [[inputX, inputY], [testX, testY]] = fashionData.load_data();
     nameY = ["T-shirt", "Trouser", "Pullover", "Dress", "Coat",\
              "Sandal", "Shirt", "Sneaker", "Bag", "Boot"];
-    inputXNorm = tf.keras.utils.normalize(inputX, axis=1);
-    testXNorm  = tf.keras.utils.normalize(testX, axis=1);
+    inputXNorm = np.array([stand2dArray(X) for X in inputX]);
+    testXNorm  = np.array([stand2dArray(X) for X in testX]);
     targetN = len(nameY);
     inputShape = [inputXNorm.shape[1], inputXNorm.shape[2], 1];   #note: need for conv2D
     inputXNorm = inputXNorm.reshape(inputXNorm.shape[0], \

KerasNN210519TestAutoencoderTest.py
@@ -25,6 +25,12 @@ from skopt.space import Integer, Real, Categorical
 
 MODELNAME="mnistFashion210519.model";
 
+def stand2dArray(array):    #following tf.image.per_image_standardization
+    array = np.array(array);
+    flatArr = array.flatten();
+    mean = np.mean(flatArr);
+    std = np.std(flatArr);
+    return (array - mean)/max(std, 1/math.sqrt(flatArr.size));
 def roundedAccuracy(yTrue, yPred):
     return tf.keras.metrics.binary_accuracy(tf.round(yTrue), tf.round(yPred))
 def buildAutoEncoder(encoder, decoder):
@@ -248,8 +254,8 @@ if __name__ == "__main__":
     [[inputX, inputY], [testX, testY]] = fashionData.load_data();
     nameY = ["T-shirt", "Trouser", "Pullover", "Dress", "Coat",\
              "Sandal", "Shirt", "Sneaker", "Bag", "Boot"];
-    inputXNorm = tf.keras.utils.normalize(inputX, axis=1);
-    testXNorm  = tf.keras.utils.normalize(testX, axis=1);
+    inputXNorm = np.array([stand2dArray(X) for X in inputX]);
+    testXNorm  = np.array([stand2dArray(X) for X in testX]);
     targetN = len(nameY);
     inputShape = [inputXNorm.shape[1], inputXNorm.shape[2], 1];   #note: needed for conv2D
     inputXNorm = inputXNorm.reshape(inputXNorm.shape[0], *inputShape);

KerasNN210617.py
@@ -43,8 +43,8 @@ def main():
         else:
             inputY.append(y);
     #####################################################
-    inputXNorm = tf.keras.utils.normalize(inputXFull, axis=1);
-    testXNorm  = tf.keras.utils.normalize(testX, axis=1);
+    inputXNorm = np.array([stand2dArray(X) for X in inputXFull]);
+    testXNorm  = np.array([stand2dArray(X) for X in testX]);
     targetN = len(nameY);
     inputShape = [inputXNorm.shape[1], inputXNorm.shape[2], 1];   #note: needed for conv2D
     inputXNorm = inputXNorm.reshape(inputXNorm.shape[0], *inputShape);
@@ -207,6 +207,12 @@ def main():
 ##############################################################################################
 ##############################################################################################
 ##############################################################################################
+def stand2dArray(array):    #following tf.image.per_image_standardization
+    array = np.array(array);
+    flatArr = array.flatten();
+    mean = np.mean(flatArr);
+    std = np.std(flatArr);
+    return (array - mean)/max(std, 1/math.sqrt(flatArr.size));
 def cloneLayer(layer):
     config = layer.get_config();
     weights = layer.get_weights();